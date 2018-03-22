29d320cb70ffa04d4498fc62aa8824cf57e5f70c
Adrien Pétremann <hello@grena.fr>
2018-03-22 17:30:59 +0100

    Add mysql

--- app.php
+++ app.php
@@ -5,6 +5,26 @@ use GuzzleHttp\Psr7\Request;
 
 include_once('vendor/autoload.php');
 
+$url = getenv('JAWSDB_MARIA_URL');
+$dbparts = parse_url($url);
+
+$hostname = $dbparts['host'];
+$username = $dbparts['user'];
+$password = $dbparts['pass'];
+$database = ltrim($dbparts['path'],'/');
+
+try {
+    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//    echo "Connected successfully";
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
+
+
 if (isset($_GET['action'])) {
     switch ($_GET['action']) {
         case 'create':
